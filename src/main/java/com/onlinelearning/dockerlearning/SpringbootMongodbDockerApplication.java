1eaa61a7f4a4bee304cda13186a2a3da2de5e38d
affanbaig111 <affanbaig111@gmail.com>
2025-04-17 15:51:07 +0530

    removed sys

--- src/main/java/com/onlinelearning/dockerlearning/SpringbootMongodbDockerApplication.java
+++ src/main/java/com/onlinelearning/dockerlearning/SpringbootMongodbDockerApplication.java
@@ -39,6 +39,7 @@ public class SpringbootMongodbDockerApplication {
     public  Book getBookById(@PathVariable Integer id) {
         Book book;
         book = bookRepo.findById(id).orElse(null);
+        System.out.println(book);
 
         return book;
 
